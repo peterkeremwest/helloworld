18d24e89fa4369437a205343e76cbf207324a34d
peter <peterkeremwest@gmail.com>
2024-03-01 10:40:17 -0500

    HTML, JAVASCRIPT AND CSS ### Implementations of Image, Text, Video and Input Forms

---
+++ hello.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('form').addEventListener('submit', function(event)
+    {
+        let name=document.querySelector('#name').value;
+        alert('hello, ' + name);
+        event.preventDefault();
+    });
+    
+});
